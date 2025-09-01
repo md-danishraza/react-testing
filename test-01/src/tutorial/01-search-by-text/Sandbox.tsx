760d4ab0185903f1a1bc102d1b113e1f3a4dee8e
Md Danish Raza <md.danish0raza@gmail.com>
2025-09-01 18:35:23 +0530

    search by text

--- test-01/src/tutorial/01-search-by-text/Sandbox.tsx
+++ test-01/src/tutorial/01-search-by-text/Sandbox.tsx
@@ -1,4 +1,31 @@
+import { useEffect,useState } from "react";
 const Sandbox = () => {
-  return <div>Sandbox</div>;
+  const [msg,setMsg] = useState(false)
+  const [error,setError] = useState(false)
+
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setMsg(true)
+    }, 500);
+
+    return ()=>clearTimeout(timer)
+  })
+  return <div>
+  
+    <h1>React Testing Library Examples</h1>
+    <p>You can search me with regular expression: 123-456-7890</p>
+
+    {
+      error ?? <p>Error message</p>
+    }
+    <ul>
+        <li>Item 1</li>
+        <li>Item 1</li>
+        <li>Item 1</li>
+      </ul>
+    {
+      msg && <p>Async message</p>
+    }
+  </div>;
 };
 export default Sandbox;
